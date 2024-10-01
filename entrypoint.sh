5d56cd278064f7e496bb7e47f3d2be7efcb5a564
Varun Verma <verma.varun@gmail.com>
2024-10-01 03:21:44 +0000

    Adding files

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "============================="
+git config --global user.name "#{GITHUB_ACTOR}"
+git config --global user.email "#{INPUT_EMAIL}"
+git config --global --ADD SAFE.DIRECTORY /GITHUB/workspace
+
+# Execute the feed.py script to create the podcast.xml file
+python3 /usr/bin/feed.py
+
+# Push the newly created file to GitHub repo
+git add -A && git commit -m "Update feed"
+git push --set-upstream origin main
+echo "============================="
